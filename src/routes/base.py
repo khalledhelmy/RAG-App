d3c3c6397ed32f741fa010397f340342ea198553
Khaled Helmy <khhelmy654@gmail.com>
2025-08-18 14:16:55 +0300

    Update Base

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,14 +1,16 @@
-from fastapi import FastAPI, APIRouter
-import os
+from fastapi import FastAPI, APIRouter, Depends
+from helpers.config import get_settings, Settings
 
 base_router = APIRouter(
-    prefix='/api/v0'
+    prefix='/api/v0',
+    tags=['api_v0']
 )
 
 @base_router.get('/')
-async def Home():
-    app_name = os.getenv('APP_NAME')
-    app_ver = os.getenv('APP_VERSION')
+async def Home(app_settings: Settings = Depends(get_settings)):
+    
+    app_name = app_settings.APP_NAME
+    app_ver = app_settings.APP_VERSION
     return {
         'App Name': app_name,
         'App Version': app_ver
