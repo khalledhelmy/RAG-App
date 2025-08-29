9fee64f52d489cd03428c9b7e2984fcfa870a682
Khaled Helmy <khhelmy654@gmail.com>
2025-08-29 18:53:56 +0300

    Create Assets files for projects

--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,9 @@ import os
 import logging
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
 
 logger = logging.getLogger('uvicorn.errors')
 
@@ -53,11 +55,22 @@ async def upload_data(request: Request, project_id: str,
                 'signal': ResponseSignal.FILE_UPLOAD_FAILED.value
             }
         )
+    
+    asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
+
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
         
     return JSONResponse(
             content={
                 'signal': ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                'file_id': file_id
+                'file_id': str(asset_resource.id)
             }
     )
 
