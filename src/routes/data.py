dae7475cf36385872a0912a93b00cb2066dc7896
Khaled Helmy <khhelmy654@gmail.com>
2025-08-29 17:10:11 +0300

    Set indices for projects and chunks

--- src/routes/data.py
+++ src/routes/data.py
@@ -22,7 +22,7 @@ data_router = APIRouter(
 async def upload_data(request: Request, project_id: str, 
                       file: UploadFile, app_settings: Settings = Depends(get_settings)):
     
-    project_model = ProjectModel(db_client=request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
 
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
@@ -68,7 +68,7 @@ async def process_endpoint(request: Request, project_id: str, response_request: 
     overlap_size = response_request.overlap_size
     do_reset = response_request.do_reset
 
-    project_model = ProjectModel(db_client=request.app.db_client)
+    project_model = await ProjectModel.create_instance(db_client=request.app.db_client)
     project = await project_model.get_project_or_create_one(project_id=project_id)
 
     process_controller = ProcessController(project_id=project_id)
@@ -98,7 +98,7 @@ async def process_endpoint(request: Request, project_id: str, response_request: 
         for i, chunk in enumerate(file_chunks)
     ]
 
-    chunk_model = ChunkModel(db_client=request.app.db_client)
+    chunk_model = await ChunkModel.create_instance(db_client=request.app.db_client)
 
     if do_reset == 1:
        _ = await chunk_model.delete_using_project_id(project_id=project.id)
